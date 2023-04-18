46c1f62aa11cfe6a039f3054c7182aa4c7e157bb
angel012912 <angel_012912@hotmail.com>
2023-04-17 20:13:28 -0600

    Decorators added

--- decorator_exercise.py
+++ decorator_exercise.py
@@ -4,21 +4,26 @@ from typing import Callable
 def star(func: Callable) -> Callable:
     """A decorator that prints 15 * before and after the function call"""
     # TODO(students): implement this function
-    pass
+    def inner(msg1, msg2, msg3):
+        print("***************")
+        func(msg1, msg2, msg3)
+        print("***************")
+    return inner
 
 
 def reverse_first_arg_string(func: Callable) -> Callable:
     """A decorator that reverses the first argument of the function call"""
     # TODO(students): implement this function
-    pass
+    def inner(msg1, msg2, msg3):
+        msg1 = msg1[::-1]
+        return func(msg1, msg2, msg3)
+    return inner
 
 
 @star
 @reverse_first_arg_string
 def printer(msg1, msg2, msg3):
-    print(msg1)
-    print(msg2)
-    print(msg3)
+    print(msg1, msg2, msg3)
 
 
 if __name__ == '__main__':
@@ -31,7 +36,7 @@ printer("i love python", "1234", "abcd")
 
 OUTPUT:
 ***************
-nohtyp evol i 1234
+nohtyp evol i 1234 
 ***************
 
 """
